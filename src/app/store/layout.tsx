29b9eba6845b1e29f1faf5da4d3d3c2a4e5039ec
Diegoabustos <balorcode22@gmail.com>
2024-03-17 14:32:08 -0600

    feat: 🎸 Styling cards and wrapper products

--- src/app/store/layout.tsx
+++ src/app/store/layout.tsx
@@ -1,26 +1,28 @@
 import { getCollections } from "app/services/shopify/collections"
 import Link from "next/link"
 
-
 export default async function Layout({ children }: { children: React.ReactNode }) {
+ const collections = await getCollections()
 
-  const collections = await getCollections()
-
-
-  return (
-    <main>
-      <nav>
-        <ul>
+ return (
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
+      <h1 className="text-4xl font-bold text-center text-gray-800 mb-10 mt-20">Explorar</h1>
+      <nav className="w-full max-w-md mx-auto">
+        <ul className="flex flex-row justify-center gap-12">
           {
             collections?.map((collection: any) => (
-              <Link key={collection.id} href={'/store/' + collection.handle} >
-                {collection.title}
-              </Link>
+              <li key={collection.id} className="mb-4">
+                <Link href={'/store/' + collection.handle}>
+                 <p className="block text-center text-gray-700 hover:text-gray-900 transition-colors duration-200 px-8 py-2 rounded-lg">
+                    {collection.title}
+                 </p>
+                </Link>
+              </li>
             ))
           }
         </ul> 
       </nav>
       {children}
     </main>
-  )
+ )
 }
--- src/components/Store/ProductCard/ProductCard.tsx
+++ src/components/Store/ProductCard/ProductCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface ProductCardInterface {
   product: ProductType;
@@ -6,19 +7,28 @@ interface ProductCardInterface {
 
 export const ProductCard = ({ product }: ProductCardInterface) => {
   return (
-    <>
-      <article className="flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden">
-        <Image
-          src={product.image}
-          alt={product.title}
-          width={500}
-          height={300}
-          objectFit="cover"
-          objectPosition="center"
-          quality={100}
-          priority
-        />
-      </article>
-    </>
-  );
+    <Link href={`/articulo/${product.handle}?id=${product.id}`}>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
+      <Image
+        src={product.image}
+        alt={product.title}
+        width={500}
+        height={300}
+        objectFit="cover"
+        objectPosition="center"
+        quality={100}
+        priority
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{product.title}</div>
+        <p className="text-gray-700 text-base">{product.description}</p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          {product.handle}
+        </span>
+      </div>
+    </div>
+    </Link>
+ );
 };
