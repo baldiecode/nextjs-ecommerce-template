8f4777bbbe1f7b1447308a9b009e4db9306848ec
Diegoabustos <balorcode22@gmail.com>
2024-03-17 13:33:50 -0600

    feat: 🎸 Testing to fetch collections data from shopify api

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,7 +1,6 @@
-import { ProductsWrapper } from "app/components/Store/ProductWrapper/ProductWrapper"
-import { getProducts } from "app/services/shopify/"
-
-
+import { ProductsWrapper } from "app/components/Store/ProductWrapper"
+import { getCollectionProducts, getCollections } from "app/services/shopify/collections"
+import { getProducts } from "app/services/shopify/products"
 
 interface CategoryProps {
   params: {
@@ -11,9 +10,17 @@ interface CategoryProps {
 }
 
 export default async function Category(props: CategoryProps) {
-  const products = await getProducts()
-  console.log(products)
+  const { categories } = props.params
+  let products = []
+  const collections = await getCollections()
+  console.log(collections)
   
+  if (categories?.length > 0) {
+    const selectedCollectionId = collections.find((collection: any) => collection.handle === categories[0]).id
+    products = await getCollectionProducts(selectedCollectionId)
+  }else {
+    products = await getProducts()
+  }
 
   return (
     <ProductsWrapper products={products} />
