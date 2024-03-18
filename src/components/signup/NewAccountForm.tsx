e609d21fb0d96c90bd1b7569d862440bc93996fc
Diegoabustos <balorcode22@gmail.com>
2024-03-17 23:10:32 -0600

    feat: 🎸 Add graphql configurate to sign up by storefront

--- src/components/signup/NewAccountForm.tsx
+++ src/components/signup/NewAccountForm.tsx
@@ -6,10 +6,10 @@ export const NewAccountForm = () => {
   const [errors, setErrors] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
-    handleCreateUser(formData);
+    await handleCreateUser(formData);
   };
 
   return (
@@ -18,57 +18,13 @@ export const NewAccountForm = () => {
         New Account
       </h1>
       <form className="grid grid-cols-2 gap-4" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="first_name"
-          placeholder="Name"
-          disabled={loading}
-          className="col-span-2 text-gray-700 font-light p-2 bg-transparent border-b border-gray-300 focus:outline-none focus:border-blue-500"
-        />
-        <input
-          type="text"
-          name="last_name"
-          placeholder="Lastname"
-          disabled={loading}
-          className="col-span-2 text-gray-700 font-light p-2 bg-transparent border-b border-gray-300 focus:outline-none focus:border-blue-500"
-        />
-        <input
-          type="text"
-          name="email"
-          placeholder="email"
-          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-          disabled={loading}
-          className="col-span-2 text-gray-700 font-light p-2 bg-transparent border-b border-gray-300 focus:outline-none focus:border-blue-500"
-        />
-        <input
-          type="text"
-          name="phone"
-          placeholder="phone number"
-          pattern="^[0-9]*$"
-          disabled={loading}
-          className="col-span-2 text-gray-700 font-light p-2 bg-transparent border-b border-gray-300 focus:outline-none focus:border-blue-500"
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="password"
-          disabled={loading}
-          className="col-span-2 text-gray-700 font-light p-2 bg-transparent border-b border-gray-300 focus:outline-none focus:border-blue-500"
-        />
-        <input
-          type="password"
-          name="password_confirmation"
-          placeholder="re-type password"
-          disabled={loading}
-          className="col-span-2 text-gray-700 font-light p-2 bg-transparent border-b border-gray-300 focus:outline-none focus:border-blue-500"
-        />
-        <input
-          type="submit"
-          name="submit"
-          value="Crear cuenta"
-          disabled={loading}
-          className="col-span-2 border border-blue-500 bg-transparent text-gray-700 font-bold uppercase p-2 rounded-lg cursor-pointer focus:outline-none"
-        />
+      <input type="text" name="firstName" placeholder="Name" disabled={loading} />
+        <input type="text" name="lastName" placeholder="Lastname" disabled={loading} />
+        <input type="text" name="email" placeholder="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" disabled={loading} />
+        <input type="text" name="phone" placeholder="phone number" pattern="^[0-9]*$" disabled={loading} />
+        <input type="password" name="password" placeholder="password" disabled={loading} />
+        <input type="password" name="password_confirmation" placeholder="re-type password" disabled={loading} />
+        <input type="submit" name="submit" value="Crear cuenta" disabled={loading} />
       </form>
       {errors.length > 0 && (
         <div>
