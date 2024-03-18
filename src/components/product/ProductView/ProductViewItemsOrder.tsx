d6de7fd92bb80f58ec2ef1b51d04b9a96017215f
Diegoabustos <balorcode22@gmail.com>
2024-03-18 15:08:59 -0600

    feat: 🎸 Implement state to handle shpping cart

--- src/components/product/ProductView/ProductViewItemsOrder.tsx
+++ src/components/product/ProductView/ProductViewItemsOrder.tsx
@@ -1,45 +1,73 @@
 "use client";
-
 import { SyntheticEvent, useState } from "react";
 import { FaCartShopping } from "react-icons/fa6";
-// import { FaCartShopping } from 'react-icons/fa';
+import { useShoppingCart } from "app/hooks/useShoppingCart";
 
 interface ProductViewItemsOrderProps {
- maxQuantity: number,
+  maxQuantity: number;
+  product: ProductType;
 }
 
-export const ProductViewItemsOrder = ({ maxQuantity }: ProductViewItemsOrderProps) => {
- const [counter, setCounter] = useState(1);
+export const ProductViewItemsOrder = ({
+  maxQuantity,
+  product,
+}: ProductViewItemsOrderProps) => {
+  const [counter, setCounter] = useState(1);
+  const { addToCart } = useShoppingCart();
 
- const handleSubmit = (event: SyntheticEvent) => {
+  const handleAddToCart = (event: SyntheticEvent) => {
     event.preventDefault();
- };
+    addToCart({
+      title: product.title,
+      price: product.price,
+      quantity: counter,
+      id: product.id,
+    });
+  };
 
- const handleSubtract = (event: SyntheticEvent) => {
+  const handleSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+  };
+
+  const handleSubtract = (event: SyntheticEvent) => {
     event.preventDefault();
     if (counter === 1) return;
     setCounter(counter - 1);
- }
+  };
 
- const handleAdd = (event: SyntheticEvent) => {
+  const handleAdd = (event: SyntheticEvent) => {
     event.preventDefault();
     if (counter === maxQuantity) return;
     setCounter(counter + 1);
- }
+  };
 
- return (
+  return (
     <div className="grid grid-cols-2 gap-3 mt-8">
       <div className="flex items-center gap-2 rounded-lg bg-gray-200">
-        <button onClick={handleSubtract} className="flex items-center justify-center h-8 w-8 text-xl font-bold text-gray-800 bg-transparent border-none cursor-pointer p-4">-</button>
+        <button
+          onClick={handleSubtract}
+          className="flex items-center justify-center h-8 w-8 text-xl font-bold text-gray-800 bg-transparent border-none cursor-pointer p-4"
+        >
+          -
+        </button>
         <p className="m-0 text-xl font-normal">{counter}</p>
-        <button onClick={handleAdd} className="flex items-center justify-center h-8 w-8 text-xl font-bold text-gray-800 bg-transparent border-none cursor-pointer p-4">+</button>
+        <button
+          onClick={handleAdd}
+          className="flex items-center justify-center h-8 w-8 text-xl font-bold text-gray-800 bg-transparent border-none cursor-pointer p-4"
+        >
+          +
+        </button>
       </div>
       <form onSubmit={handleSubmit} className="w-full">
-        <button type="submit" className="w-full h-12 flex items-center justify-center gap-2 bg-gradient-to-r from-purple-600 to-blue-600 text-white font-bold text-xl rounded-lg p-2 mt-4">
+        <button
+          type="submit"
+          className="w-full h-12 flex items-center justify-center gap-2 bg-gradient-to-r from-purple-600 to-blue-600 text-white font-bold text-xl rounded-lg p-2 mt-4"
+          onClick={handleAddToCart}
+        >
           <FaCartShopping />
           <span>Add to cart</span>
         </button>
       </form>
     </div>
- )
+  );
 };
--- src/components/shared/ShoppingCart/ShoppingCart.tsx
+++ src/components/shared/ShoppingCart/ShoppingCart.tsx
@@ -1,17 +1,36 @@
 "use client";
+import { useShoppingCart } from "app/hooks/useShoppingCart";
 import { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
 
+
 export const ShoppingCart = () => {
- const [countItems, setCountItems] = useState(0);
+
+  const { cart } = useShoppingCart();
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => setIsOpen(!isOpen);
+
 
  return (
-    <button className="relative bg-transparent border-none cursor-pointer text-gray-700">
+    <button className="relative bg-transparent border-none cursor-pointer text-gray-700" onClick={handleOpen} >
+      <span className="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full p-1">
+        {cart.length}
+      </span>
       <FaShoppingCart className="w-8 h-8 text-gray-700" />
-      {countItems > 0 && (
-        <span className="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full p-1">
-          {countItems}
-        </span>
+      {isOpen && (
+        <div className="absolute bg-primary border border-gray-300 rounded-lg right-0 max-w-[320px] min-w-[250px] p-4">
+          {
+            cart.map(item => (
+              <>
+                <p key={item?.id}>{item?.title}</p>
+                <p>Cantidad: {item.quantity}</p>
+              </>
+            ))
+          }
+          <button className="bg-main-contrast text-text-color w-full py-1 rounded-lg border-none font-bold cursor-pointer">
+            Buy
+          </button>
+        </div>
       )}
     </button>
  );
