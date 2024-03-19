11f1b4ca516525761e86d866093ce0e75f237fba
Diegoabustos <balorcode22@gmail.com>
2024-03-19 12:26:40 -0600

    refactor: 💡 changing product card style

--- src/components/Store/ProductCard/ProductCard.tsx
+++ src/components/Store/ProductCard/ProductCard.tsx
@@ -8,27 +8,31 @@ interface ProductCardInterface {
 export const ProductCard = ({ product }: ProductCardInterface) => {
   return (
     <Link href={`/product/${product.handle}?id=${product.id}`}>
-    <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
-      <Image
-        src={product.image}
-        alt={product.title}
-        width={500}
-        height={300}
-        objectFit="cover"
-        objectPosition="center"
-        quality={100}
-        priority
-      />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{product.title}</div>
-        <p className="text-gray-700 text-base">{product.description}</p>
-      </div>
-      <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          {product.handle}
-        </span>
-      </div>
-    </div>
+      <article className="flex flex-col bg-white shadow-md min-h-[400px]">
+        <div className="relative flex-grow">
+          <Image
+            src={product.image}
+            alt={product.title}
+            width={500}
+            height={300}
+            objectFit="cover"
+            objectPosition="center"
+            quality={100}
+            priority
+            className="h-96"
+          />
+        </div>
+        <div className="px-4 py-4 justify-between flex flex-row">
+          <div className="text-base truncate overflow-ellipsis mb-2 w-36">
+            {product.title}
+          </div>
+          
+          <p className="text-gray-700 text-lg font-semibold">
+            {product.price}
+          </p>
+        </div>
+      </article>
+
     </Link>
- );
+  );
 };
--- src/components/Store/ProductWrapper/ProductWrapper.tsx
+++ src/components/Store/ProductWrapper/ProductWrapper.tsx
@@ -6,7 +6,7 @@ interface ProductsWrapperProps {
 
 export const ProductsWrapper = ({ products }: ProductsWrapperProps) => {
  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-6">
+    <div className="flex flex-col items-center justify-center min-h-screen py-6">
       <h1 className="text-3xl font-bold text-gray-800 mb-8">Nuestros Productos</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products.map((product) => (
