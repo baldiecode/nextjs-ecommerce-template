a22c77822794131eb385fd7313b1ed0cc6993be0
Diegoabustos <balorcode22@gmail.com>
2024-03-19 09:42:53 -0600

    refactor: 💡 Modify header styles

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,38 +1,44 @@
 import Link from "next/link";
 import { validateAccessToken } from "app/utils/auth/validateAccessToken";
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 
-const NoSSRShoppingCart = dynamic(() => import('../ShoppingCart'), { ssr: false })
-
+const NoSSRShoppingCart = dynamic(() => import("../ShoppingCart"), {
+  ssr: false,
+});
 
 export const Header = async () => {
-
-  const customer = await validateAccessToken()
-  
+  const customer = await validateAccessToken();
 
   return (
-      <header className="bg-gray-100 text-gray-800 shadow-lg p-4 fixed top-0 w-full z-50">
-        <div className="container mx-auto">
-          <div className="flex justify-between items-center">
-            <div className="text-lg">Mi Tienda Online</div>
-            <nav className="ml-auto">
-              <ul className="flex space-x-4">
+    <header className="bg-gray-100 text-gray-800 shadow-lg p-4 fixed top-0 w-full z-50">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <div className="text-lg font-semibold">Mi Tienda Online</div>
+          <nav>
+            <ul className="flex space-x-8">
+              {" "}
               <li>
-                  <Link href="/" className="hover:text-blue-950">Inicio</Link>
-                </li>
-                <li>
-                  <Link href="/store" className="hover:text-blue-950">Tienda</Link>
-                </li>
-                <li>
-                  <a href="/login" className="hover:text-blue-950">Iniciar Sesión</a>
-                </li>
-              </ul>
-              {customer?.firstName ? (<p>Hola! {customer.firstName}</p>) : (<Link href="/login">Login</Link>)}
+                <Link href="/" className="hover:text-blue-950">
+                  Inicio
+                </Link>
+              </li>
+              <li>
+                <Link href="/store" className="hover:text-blue-950">
+                  Tienda
+                </Link>
+              </li>
+              {customer?.firstName ? (
+                <p className="text-sm">Hola! {customer.firstName}</p>
+              ) : (
+                <Link href="/login" className="hover:text-blue-950">
+                  Login
+                </Link>
+              )}
               <NoSSRShoppingCart />
-            </nav>
-          </div>
+            </ul>
+          </nav>
         </div>
-      </header>
+      </div>
+    </header>
   );
- }
- 
+};
